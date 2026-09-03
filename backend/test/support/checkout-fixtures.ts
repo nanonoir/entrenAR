6c977258f46f701e9d38e4f3fd2bc56a50ba13f9
Nahuel Noir <144752089+nanonoir@users.noreply.github.com>
2026-09-03 01:22:26 -0300

    refactor(checkout): isolate unit test fixtures

---
+++ backend/test/support/checkout-fixtures.ts
@@ -0,0 +1,5 @@
+export * from "./checkout-database-fixtures";
+export * from "./checkout-http.helpers";
+export * from "./checkout-http.types";
+export * from "./checkout-domain-fixtures";
+export * from "./checkout-unit-fixtures";
